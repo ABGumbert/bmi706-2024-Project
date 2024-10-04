6846f159bb81cc40308c8246e7be8bf939e1f1a2
Andrew Gumbert <andrew_gumbert@hms.harvard.edu>
2024-10-04 15:47:03 -0400

    Update cirrhosis_streamlit_app.py

--- cirrhosis_streamlit_app.py
+++ cirrhosis_streamlit_app.py
@@ -138,7 +138,7 @@ def time_series_chart_race(data, selectors):
     ).add_selection(
         selectors[2]
     ).transform_filter(
-        selectors[0] & selectors[1] & selectors[2]
+        selectors[2]
     ).properties(
         width=600,
         height=500,
