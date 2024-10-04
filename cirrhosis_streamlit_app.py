58fd2963c73732d42751af4196d9c4ce162210c6
Andrew Gumbert <andrew_gumbert@hms.harvard.edu>
2024-10-04 16:31:40 -0400

    Update cirrhosis_streamlit_app.py

--- cirrhosis_streamlit_app.py
+++ cirrhosis_streamlit_app.py
@@ -156,7 +156,7 @@ def distribution_boxplot(data):
     # Credit to https://altair-viz.github.io/user_guide/marks/boxplot.html
     # for helping with mark_boxplot
     return alt.Chart(data_subset).mark_boxplot().encode(
-        x = alt.X('val:Q', scale=alt.Scale(type='log',domain=(0, 0.01), clamp=True))
+        x = alt.X('val:Q').scale(type="log")
     ).properties(
         width=600,
         height=200,
