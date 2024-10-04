0d93a7b36f52c5a2ccc38ab630c8ebe0c4fa8657
Andrew Gumbert <andrew_gumbert@hms.harvard.edu>
2024-10-04 17:05:26 -0400

    Update cirrhosis_streamlit_app.py

--- cirrhosis_streamlit_app.py
+++ cirrhosis_streamlit_app.py
@@ -261,6 +261,8 @@ def display_charts(data):
     st.altair_chart(time_series_chart_race(data, selector_race), use_container_width=True)
 
     st.altair_chart(distribution_boxplot(data), use_container_width=True)
+
+    print(selector_age)
     st.altair_chart(selected_distribution_boxplot(data, [selector_age, selector_sex, selector_race]), use_container_width=True)
     
 
